60009224551771ab5740fba7bd285874240faffa
Rubén Gil <rubengil22@gmail.com>
2014-01-21 12:35:11 +0100

    Removed access_token parameter from linked_in_client helper method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,16 +3,15 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   setup do
     @user = users :rubengil
-    @access_token = "ACCESS_TOKEN"
   end
 
   test "Retrieves user if exists" do
-    @linked_in_client = linked_in_client @access_token, @user.uid
+    @linked_in_client = linked_in_client @user.uid
     assert_equal @user, User.from_linked_in(@linked_in_client)
   end
 
   test "Creates user and profile if it doesn't exist" do
-    @linked_in_client = linked_in_client @access_token, "new_user_id", {first_name: "Pepe", summary: "Pepe is very inexperienced"}
+    @linked_in_client = linked_in_client "new_user_uid", {first_name: "Pepe", summary: "Pepe is very inexperienced"}
     assert_difference "User.count" do
       assert_difference "Profile.count" do
         User.from_linked_in(@linked_in_client)
@@ -20,8 +19,8 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  def linked_in_client access_token, uid, user_fields = nil
-    linked_in_client = LinkedIn::Client.new(access_token, "expires_in")
+  def linked_in_client uid, user_fields = nil
+    linked_in_client = LinkedIn::Client.new("ACCESS_TOKEN", "expires_in")
     linked_in_client.expects(:get_user_info).returns(user_info_hash uid)
     if user_fields
       linked_in_client.expects(:get).returns(profile_info_hash uid, user_fields)
