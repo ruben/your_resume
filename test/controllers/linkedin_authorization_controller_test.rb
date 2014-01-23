1d3d5cbe7cacb76db99a4009cd9aec408d20de2f
Rubén Gil <rubengil22@gmail.com>
2014-01-23 12:37:57 +0100

    Refactored to use stubbed linked in client

--- test/controllers/linkedin_authorization_controller_test.rb
+++ test/controllers/linkedin_authorization_controller_test.rb
@@ -3,13 +3,12 @@ require 'test_helper'
 class LinkedinAuthorizationControllerTest < ActionController::TestCase
   setup do
     @code, @state = "CODE", "STATE"
-    @access_token = "ACCESS_TOKEN"
     @user = users :rubengil
+    stub_linked_in_client
   end
 
   test "signs in and redirects to root path when authorized" do
-    LinkedIn::OauthClient.any_instance.stubs(:access_token).returns(JSON.parse access_token_hash @access_token)
-    LinkedIn::Client.any_instance.stubs(:user_info).returns(JSON.parse user_info_hash @user.uid, @user.first_name, @user.last_name, @user.email, @user.access_token, @user.expires_at)
+    LinkedIn::OauthClient.any_instance.stubs(:access_token).returns(JSON.parse access_token_hash @user.access_token)
     get :callback, code: @code, state: @state
     assert warden.authenticated?(:user)
     assert_redirected_to root_path
