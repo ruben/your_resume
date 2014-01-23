e6b9e02e391b93f62e5f91a9ff272d18d0558d8e
Rubén Gil <rubengil22@gmail.com>
2014-01-23 09:49:24 +0100

    Refactored ProfileTest

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,12 +1,14 @@
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
+  setup do
+    @client = mock('client')
+    @profile = profiles :rubengil_profile
+  end
 
   test "Loads data from LinkedIn client" do
-    client = LinkedIn::Client.new "access_info", "expires_in"
-    client.stubs(:get_profile_info).returns('{"firstName": "Rubén", "summary": "Rubén has a looooot of experience"}')
-    @profile = profiles :rubengil_profile
-    @profile.load_from(client)
+    @client.stubs(:profile_info).returns(JSON.parse '{"firstName": "Rubén", "summary": "Rubén has a looooot of experience"}')
+    @profile.load_from(@client)
     assert_equal "Rubén", @profile.first_name
     assert_equal "Rubén has a looooot of experience", @profile.summary
   end
