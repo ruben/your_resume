d0818a6d036d6fadc33a3483e2bc7d63cebccdb7
Rubén Gil <rubengil22@gmail.com>
2014-01-27 10:41:32 +0100

    Fixed bug in oauth_client

--- lib/linked_in/oauth_client.rb
+++ lib/linked_in/oauth_client.rb
@@ -38,7 +38,7 @@ module LinkedIn
     def authorize_params
       {response_type: "code",
        client_id: "0sskhvc5i3a3",
-       scope: "r_basicprofile r_emailaddress",
+       scope: "r_fullprofile r_emailaddress",
        state: "5c3dfc38244eeea97e425c8ae79c6a13f08c82182f03d3e29b2b5534ae1c78ad2595e9f253fad9ec4429dda90412d64166fafdef56ac8c8be76e0c0487c94031",
        redirect_uri: redirect_uri,
       }
