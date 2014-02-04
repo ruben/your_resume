6c7c29b23e247042a12e4ef78add9c136e53c4fb
Rubén Gil <rubengil22@gmail.com>
2014-02-04 12:20:52 +0100

    Extracted period helper

--- app/helpers/resumes_helper.rb
+++ app/helpers/resumes_helper.rb
@@ -2,4 +2,8 @@ module ResumesHelper
   def render_section header, collection
     render partial: 'section', locals: { header: header, collection: collection }
   end
+
+  def period(start_date, end_date)
+    [start_date, end_date].compact.map {|date| I18n.l date}.join(' - ')
+  end
 end
