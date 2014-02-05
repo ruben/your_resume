4bc8db497a4547191fdbe038787c7a30fce97239
Rubén Gil <rubengil22@gmail.com>
2014-02-05 10:07:53 +0100

    Renamed @user_profile to @profile

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -2,7 +2,7 @@ class ResumesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user_profile = current_user.profile
+    @profile = current_user.profile
   end
 
   def refresh
